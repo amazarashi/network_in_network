1c5e64d985c307733a959777c58a157b4d56a982
suguru <u.sgr.22114w@gmail.com>
2017-04-02 14:21:35 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# About
+
+Network In Network by chainer
+
+# Paper
+
+[Network In Network](https://arxiv.org/abs/1312.4400)
+
+# Model
+
+Network In Network
+
+# How to run
+
+git clone git@github.com:amazarashi/network_in_network.git
+
+cd ./network_in_network
+
+python main.py -g 1
+
+# Inspection
+
+### dataset
+Cifar10 [(link)](https://www.cs.toronto.edu/~kriz/cifar.html)
+
+### Result
+
+<!-- (2) optimizer: MomentumSGD
+  - weight decay : 1.0e-4
+  - momentum : 0.9
+  - schedule[default:0.1,150:0.01,225:0.001]
+
+
+![accuracy-adam](https://github.com/amazarashi/squeeze-chainer/blob/develop/result/momsgd/accuracy.png "accuracy")
+
+![loss-adam](https://github.com/amazarashi/squeeze-chainer/blob/develop/result/momsgd/loss.png "loss") -->
---
+++ network_in_network.py
@@ -0,0 +1,51 @@
+import chainer
+import chainer.functions as F
+import chainer.links as L
+import skimage.io as io
+import numpy as np
+from chainer import utils
+import math
+
+class Network_in_Network(chainer.Chain):
+
+    def __init__(self,category_num=10):
+        initializer = math.sqrt(2)
+        super(Network_in_Network,self).__init__(
+            mlp1 = L.MLPConvolution2D(3,(64,96,128),3,stride=2,pad=0,wscale=initializer),
+            mlp2 = L.MLPConvolution2D(128,(256,256,256),3,stride=2,pad=0,wscale=initializer),
+            mlp3 = L.MLPConvolution2D(256,(256,256,category_num),3,stride=2,pad=0,wscale=initializer)
+        )
+
+    def __call__(self,x,train=True):
+        #x = chainer.Variable(x)
+        h = F.relu(self.mlp1(x))
+        h  = F.dropout(h,ratio=.5,train=train)
+        h = F.max_pooling_2d(h,3,stride=2,pad=0)
+
+        h = F.relu(self.mlp2(h))
+        h  = F.dropout(h,ratio=.5,train=train)
+        h = F.max_pooling_2d(h,3,stride=2,pad=0)
+
+        h = F.relu(self.mlp3(h))
+        h  = F.dropout(h,ratio=.5,train=train)
+        h = F.max_pooling_2d(h,3,stride=2,pad=0)
+
+        num, categories, y, x = h.data.shape
+        h = F.reshape(F.average_pooling_2d(h,(y, x)), (num, categories))
+        return h
+
+    def calc_loss(self,y,t):
+        loss = F.softmax_cross_entropy(y,t)
+        return loss
+
+    def accuracy_of_each_category(self,y,t):
+        y.to_cpu()
+        t.to_cpu()
+        categories = set(t.data)
+        accuracy = {}
+        for category in categories:
+            supervise_indices = np.where(t.data==category)[0]
+            predict_result_of_category = np.argmax(y.data[supervise_indices],axis=1)
+            countup = len(np.where(predict_result_of_category==category)[0])
+            accuracy[category] = countup
+        return accuracy
